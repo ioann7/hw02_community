20b6704b250be5296e99e12d8194d7d42388152a
ioann7 <ioann.chimrov@gmail.com>
2022-09-08 11:28:16 +0300

    added signup

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls, name='admin'),
-    path('auth/', include('users.urls')),
+    path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
 ]
